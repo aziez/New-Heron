48da4099d24675aa5afbd48d4d56b54b918438a7
Aziez <aziz13.id@gmail.com>
2024-03-07 14:50:55 +0700

    finallitation and prepare android version

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,17 +1,54 @@
 import {
+  IonAlert,
   IonContent,
   IonHeader,
   IonPage,
   IonTitle,
   IonToolbar,
+  useIonRouter,
 } from "@ionic/react";
 import ExploreContainer from "../../components/ExploreContainer";
 import "./Home.css";
 import MainPages from "../../components/Pages/Page";
+import { App } from "@capacitor/app";
+import { useEffect, useState } from "react";
 
 const Home: React.FC = () => {
+  const [showAlert, setShowAlert] = useState(false);
+  const router = useIonRouter();
+
+  // Use `ionBackButton` event within a React context
+  useEffect(() => {
+    const backButtonEvent = document.addEventListener(
+      "ionBackButton",
+      (evy: any) => {
+        evy.detail.register(-1, () => {
+          if (!router.canGoBack()) {
+            setShowAlert(true);
+          }
+        });
+      }
+    );
+
+    // Cleanup event listener on unmount
+  }, []);
+
   return (
     <MainPages backgroundUrl="gate.jpg">
+      <IonAlert
+        isOpen={showAlert}
+        header="Are you sure?"
+        message="Do you really want to exit the app?"
+        buttons={[
+          {
+            text: "No",
+            role: "cancel",
+            cssClass: "secondary",
+            handler: () => setShowAlert(false),
+          }, // Close alert on "No" button
+          { text: "Yes", handler: () => App.exitApp() },
+        ]}
+      />
       <ExploreContainer />
     </MainPages>
   );
